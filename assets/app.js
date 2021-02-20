de7c6c10aa4ceed3c344e826e9816d54dcb93215
Ross Keatinge <ross@thirdandgrove.com>
2021-02-19 20:42:03 -0600

    React Hello World!

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const appElement = document.getElementById('app');
+
+const template = <p>Hello World!</p>;
+
+ReactDOM.render(template, appElement);
